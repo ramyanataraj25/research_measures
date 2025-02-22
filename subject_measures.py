9500909ab18a1a564e52544b8e552ed9c506af81
Ramya Nataraj <ramyan25@terpmail.umd.edu>
2025-02-22 12:20:47 -0500

    adding subject csvs and edits to subject_measures file

---
+++ subject_measures.py
@@ -0,0 +1,28 @@
+# import libraries that are necessary to write code
+
+# read the subject file in and parse through removing "ɚɹ" the ɹ is it follows
+# ɚ	or 3
+def edit_pronunciations(ntr_task_file):
+    word = "" # can ingore these variables was trying to remove the error line
+    return word
+    
+
+def create_new_subject_file(ntr_task_file, subject_file):
+    file = subject_file # this is wrong assignment just removing the error line
+    
+# open the ntr_task_file file using a with statement
+
+# open subject_file file with a with statement to write to it
+
+    # create a list that can store all the subject pronunciations
+    
+    # append the 'concatentate' column in order to the first list of pronunciations
+    
+    # write to the second file to contain all concatenated pronunciations
+    # and words
+    
+    return file
+    
+
+# take file created from previous file (so subject_file) and apply conversion 
+# code to convert IPA to toolkit conventions (from Caleb)
