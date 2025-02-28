7bbb73821e71d3a1189822d5a6e90caaffbb3170
Ramya Nataraj <ramyan25@terpmail.umd.edu>
2025-02-28 14:53:32 -0500

    .

---
+++ subjects_measures.py
@@ -0,0 +1,15 @@
+def create_new_subject_file(ntr_task_file, subject_file):
+    file = subject_file # this is wrong assignment just removing the error line
+    
+# open the ntr_task_file file using a with statement
+
+# open subject_file file with a with statement to write to it
+
+    # create a list that can store all the subject pronunciations
+    
+    # append the 'concatentate' column in order to the first list of pronunciations
+    
+    # write to the second file to contain all concatenated pronunciations
+    # and words
+    
+    return file
