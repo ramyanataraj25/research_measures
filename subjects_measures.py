c22321da6d366e2d7dfe8c1308f22e709b7fe356
ramya_nataraj <ramyanataraj25@gmail.com>
2025-03-19 15:39:31 -0400

    new change

--- subjects_measures.py
+++ subjects_measures.py
@@ -2,7 +2,7 @@
 
 # read the subject file in and parse through removing "ɚɹ" the ɹ is it follows
 # ɚ	or 3
-def edit_pronunciations(subject_file):
+def edit_pronunciations(concatenations):
     word = "" # can ingore these variables was trying to remove the error line
     return word
 
